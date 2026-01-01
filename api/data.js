dfcdda3c6d95df1b8413e98f9a1f0b1a7612dc0b
danielillich <danielillich2@gmail.com>
2026-01-01 19:55:25 +0100

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,8 +1,6 @@
-// In-memory storage for participants data
-let participants = [];
+const storage = require('./storage');
 
 module.exports = (req, res) => {
-  // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -13,6 +11,7 @@ module.exports = (req, res) => {
 
   if (req.method === 'GET') {
     try {
+      const participants = storage.getParticipants();
       return res.status(200).json(participants);
     } catch (error) {
       console.error('Error in data.js:', error);
